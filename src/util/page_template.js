6bc325931e18a026e5f58d2a5dbe075607faadbf
Caspar Jeffrey <caspar@sol.com.au>
2019-04-22 22:07:25 +1000

    commit changes to template

--- src/util/page_template.js
+++ src/util/page_template.js
@@ -59,12 +59,26 @@ const whiteSpacePre = {
   whiteSpace: 'pre-wrap'
 }
 
+const hoveredWhiteSpacePre = {
+  whiteSpace: 'pre-wrap',
+  backgroundColor: '#CCC'
+}
+
 const objectText = {
   fontFamily: 'monospace, monospace'
 }
 
 const textLine = {
+  display: 'flex',
+  alignItems: 'center',
   height: '40px'
+}
+
+const hoveredTextLine = {
+  display: 'flex',
+  alignItems: 'center',
+  height: '40px',
+  backgroundColor: '#CCC'
 }
 
 const type = ${type}
@@ -76,9 +90,14 @@ function ${name}(props) {
   const [mobileOpen, setMobileOpen] = useState(false);
   const [schemaData, setSchemaData] = useState([])
   const [fields, updateFields] = useState(parse(type, 1))
+    const [hovering, setHovering] = useState('')
 
   function handleDrawerToggle() {
     setMobileOpen(!mobileOpen);
+  }
+
+  const hovered = row => {
+    return (typeof hovering == 'object' && hovering) ? (row > hovering[0]+0.99 && row < hovering[1]) : row == hovering
   }
 
   const drawer = (
@@ -155,29 +174,49 @@ function ${name}(props) {
           </div>
           {
             fields && fields.map((textRow, i) =>
-              <div key={i} style={textLine}>
-                {
-                  textRow.map((chunk, j) => {
-                    let item, args, clickFunction
-                    if (typeof chunk == 'string'){
-                      item = chunk
-                      args = [item, i, j, fields, dataObject]
-                      clickFunction = expand
-                    } else {
-                      item = chunk.text
-                      args = [chunk.collapse, fields, i]
-                      clickFunction = collapse
+              {
+                return <div
+                  key={i}
+                  style={ hovered(i) ? hoveredTextLine : textLine}
+                  onClick={() => {
+                    if(typeof fields[i][0] !== 'string'){
+                      updateFields(collapse(fields[i][0].collapse, fields, i))
+                      setHovering(null)
                     }
-                      return <span
-                        key={j + 'span'}
-                        style={whiteSpacePre}
-                        onClick={() => updateFields(clickFunction(...args))}
-                        >
-                        {item}
-                      </span>
-                  })
-                }
-              </div>
+                  }}
+                  onMouseEnter={() => setHovering(
+                    fields[i][0].collapse ? [
+                      i-fields[i][0].collapse.offset-1,
+                      i+fields[i][0].collapse.length-fields[i][0].collapse.offset,
+                    ] : i
+                  )}
+                  onMouseLeave={() => setHovering(null)}
+                  >
+                  {
+                    textRow.map((chunk, j) => {
+                      let item, args, clickFunction
+                      if (typeof chunk == 'string'){
+                        item = chunk
+                        args = [item, i, j, fields, dataObject]
+                        clickFunction = expand
+                      } else {
+                        item = chunk.text
+                        args = [chunk.collapse, fields, i]
+                        clickFunction = collapse
+                      }
+                        return <span
+                          key={j + 'span'}
+                          style={ hovered(i + '.' + j) ? hoveredWhiteSpacePre : whiteSpacePre}
+                          onClick={() => updateFields(clickFunction(...args))}
+                          onMouseEnter={() => setHovering(i + '.' + j)}
+                          onMouseLeave={() => setHovering(i)}
+                          >
+                          {item}
+                        </span>
+                    })
+                  }
+                </div>
+              }
             )
           }
           <div style={textLine}>
