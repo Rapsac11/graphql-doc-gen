f835b243d4212c440fc2d006f756af30c7111ffd
Caspar Jeffrey <caspar@sol.com.au>
2019-04-27 22:49:36 +1000

    remove redundancies

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import React from "react"
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from '@material-ui/core/CssBaseline';
+import "./src/styles/global.css"
 
 const theme = createMuiTheme({ typography: { useNextVariants: true } });
 
