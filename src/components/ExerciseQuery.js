84480232455bf20a48c56327576120931b32e81f
Caspar Jeffrey <caspar@sol.com.au>
2019-04-28 23:05:53 +1000

    add back button to query veiw

--- src/components/ExerciseQuery.js
+++ src/components/ExerciseQuery.js
@@ -2,20 +2,38 @@ import React, { useState, useReducer } from 'react'
 import QueryEditorBlade from './QueryEditorBlade'
 import QueryResponseBlade from './QueryResponseBlade'
 import { QueryReducer, QueryDispatchContext, QueryResponseContext } from '../reducers'
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import Button from '@material-ui/core/Button';
 
 const main = {
   height: `calc(100% - 64px)`,
   display: 'flex'
 }
 
+const returnButtonContainer = {
+  width: '10px',
+  marginRight: '24px'
+}
+
+const returnButton = {
+  height: '100%',
+  padding: '0px',
+  minWidth: '10px',
+  borderRadius: '10px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '#ccc'
+}
+
 const left = {
   borderRight: '0px solid rgba(0, 0, 0, 0.12)',
-  width: '50%',
+  width: 'calc(50% - 17px)',
   height: '100%'
 }
 
 const right = {
-  width: '50%',
+  width: 'calc(50% - 17px)',
   height: '100%',
   paddingLeft: '14px',
   backgroundColor: '#eaeaea',
@@ -29,6 +47,11 @@ export default props => {
     <QueryDispatchContext.Provider value={dispatch}>
       <QueryResponseContext.Provider value={response}>
         <div style={main}>
+          <div style={returnButtonContainer}>
+            <Button style={returnButton}>
+              <ChevronLeft />
+            </Button>
+          </div>
           <div style={left}>
             <QueryEditorBlade />
           </div>
