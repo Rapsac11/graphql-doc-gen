0ebf4eeefa9c9683c52b24644f3b71c9c8c87fd9
Caspar Jeffrey <caspar@sol.com.au>
2019-05-19 17:13:10 +1000

    correct deplyment

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,16 @@
-import React from "react"
+import React, { useReducer } from "react"
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import "./src/styles/global.css"
+
+const theme = createMuiTheme({ typography: { useNextVariants: true } });
 
 export const wrapRootElement = ({ element }) => {
-  return (element)
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {element}
+    </ThemeProvider>
+  )
 }
